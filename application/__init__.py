30b6c882e1bc9b96a44863bec59fb3d11beef2ae
JuissiJohtaja <juha.koivu@outlook.com>
2020-03-25 20:37:41 +0200

    heroku db

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 # tietokanta
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pizzapalvelu.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pizzapalvelu.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -38,4 +44,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # luodaan taulut tietokantaan tarvittaessa
-db.create_all()
+try: 
+    db.create_all()
+except:
+    pass

ab9d7c59398169790a4ad62990ac6b4e559652c0
JuissiJohtaja <juha.koivu@outlook.com>
2020-03-29 19:53:03 +0300

    logged user, forms fixed, cancel added

--- application/users/views.py
+++ application/users/views.py
@@ -1,7 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user, login_required
 
-  
 from application import app, db
 from application.auth.models import User
 #from application.auth.forms import LoginForm, SignupForm

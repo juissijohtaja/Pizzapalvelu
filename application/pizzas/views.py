4523112a3709581058f565ef11cbd27c63830ae4
JuissiJohtaja <juha.koivu@outlook.com>
2020-04-03 18:36:31 +0300

    pizza

--- application/pizzas/views.py
+++ application/pizzas/views.py
@@ -8,6 +8,7 @@ from flask import redirect, render_template, request, url_for
 from flask_login import login_required
 
 @app.route("/pizzat/", methods=["GET"])
+@login_required
 def pizzas_index():
     return render_template("pizzas/list.html", pizzas = Pizza.query.all())
 
@@ -23,23 +24,24 @@ def pizzas_get_item(pizza_id):
     return render_template("pizzas/pizza.html", pizza = t, form = PizzasEditForm())
 
 @app.route("/pizzat/<pizza_id>/", methods=["POST"])
+@login_required
 def pizzas_set_item(pizza_id):
 
     p = Pizza.query.get(pizza_id)
-    #form = PizzasEditForm(request.form)
+    form = PizzasEditForm(request.form)
 
-    #if not form.validate():
-        #return render_template("pizzas/pizza.html", pizza = p, form = form, toppings = Topping.query.all(), error = "Tarkista lomake.")
+    if not form.validate():
+        return render_template("pizzas/pizza.html", pizza = p, form = form, toppings = Topping.query.all(), error = "Tarkista lomake.")
 
     p.name = request.form.get("name")
     p.price = request.form.get("price")
     p.toppings = []
 
     topping_ids = []
-    topping_ids.append(request.form.get("topping1"))
-    topping_ids.append(request.form.get("topping2"))
-    topping_ids.append(request.form.get("topping3"))
-    topping_ids.append(request.form.get("topping4"))
+    topping_ids.append(request.form.get('topping1', type=int))
+    topping_ids.append(request.form.get('topping2', type=int))
+    topping_ids.append(request.form.get('topping3', type=int))
+    topping_ids.append(request.form.get('topping4', type=int))
 
     for id in topping_ids:
         topping=Topping.query.get(id)

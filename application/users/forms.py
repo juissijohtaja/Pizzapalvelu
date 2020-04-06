04c40eed6ba9aee31383919da4bd8dc8ece5765c
JuissiJohtaja <juha.koivu@outlook.com>
2020-04-06 13:27:50 +0300

    forms layout

--- application/users/forms.py
+++ application/users/forms.py
@@ -6,7 +6,7 @@ class UserEditForm(FlaskForm):
     phone = IntegerField("Puhelinnumero", render_kw={'class':'form-control form-control-sm'})
     address = StringField("Osoite", [validators.Length(min=2)], render_kw={'class':'form-control form-control-sm'})
     username = StringField("Käyttäjätunnus", [validators.Length(min=2)], render_kw={'class':'form-control form-control-sm'})
-    password = PasswordField("Salasana", [validators.Length(min=2)], render_kw={'class':'form-control form-control-sm'})
+    password = StringField("Salasana", [validators.Length(min=2)], render_kw={'class':'form-control form-control-sm'})
   
     class Meta:
         csrf = False

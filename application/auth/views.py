7034b3b1fc1a1ca03874cc84ae6a592a77a475e5
JuissiJohtaja <juha.koivu@outlook.com>
2020-04-15 20:07:29 +0300

    navbar design

--- application/auth/views.py
+++ application/auth/views.py
@@ -15,7 +15,7 @@ def auth_login():
 
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
-        return render_template("auth/loginform.html", form = form, error = "Käyttäjää ei löydy.")
+        return render_template("auth/loginform.html", form = form, error = "Käyttäjätunnus tai salasana on väärin.")
 
     login_user(user)
     return redirect(url_for("index"))

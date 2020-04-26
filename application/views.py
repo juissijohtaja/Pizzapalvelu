7dc9f5e3e8af20825f2315ccda3f72e2938bd31c
JuissiJohtaja <juha.koivu@outlook.com>
2020-04-26 17:45:42 +0300

    menu added

--- application/views.py
+++ application/views.py
@@ -2,13 +2,16 @@ from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.pizzas.models import Pizza
 from application.toppings.models import Topping
+from itertools import islice
 
 from application.pizzatoppings.models import PizzaTopping
 
 @app.route("/", methods=["GET"])
 def index():
     pizzas = Pizza.query.all()
+    pizzasRecommended = islice(pizzas, 3)
+    pizzas.sort(key=lambda x: x.price)
     pwp = Pizza.find_pizzas_with_pineapple()
     pwc = Pizza.find_pizzas_with_chili()
-    return render_template("index.html", pizzas = pizzas, pineapplePizzas = pwp, chiliPizzas = pwc)
+    return render_template("index.html", pizzas = pizzas, pizzasRecommended = pizzasRecommended, pineapplePizzas = pwp, chiliPizzas = pwc)
     
